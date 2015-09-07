f22f86833f047afa71074c72e1080620227d32f5
Michael Haberler <git@mah.priv.at>
2015-09-07 15:05:57 +0200

    canopen_bus: on creation, open an interface
    provide timeout and process methods

--- canopen_bus.py
+++ canopen_bus.py
@@ -1,20 +1,33 @@
 ''' canopen bus class '''
 
+import can
+from can.interfaces.interface import Bus
+
 class CanopenBus:
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, ifname="can0", bustype="socketcan_ctypes"):
+        self.ifname = ifname
+        self.bus = Bus(ifname, bustype=bustype)
+        if self.bus.socket < 0:
+            raise IOError,"could not open %s" % ifname
         self.devices = dict() # node-id -> device object dict
+
+    def fd(self):  # return the SocketCAN file descriptor
+        return self.bus.socket
+
+    def process(self, msg):
+        # see https://python-can.readthedocs.org/en/latest/message.html
+        print "process: %s  %s" % (self.ifname,str(msg))
+
+    def timeout(self):
+        print "timeout: %s" % (self.ifname)
 
     def add_device(self,node_id, device):
         self.devices[node_id] = device
-
-    def displayname(self):
-        print "name: ", self.name
 
     def num_devices(self):
         return len(self.devices.keys())
 
     def __str__(self):
-        s = "CanopenBus: name=%s " % self.name
+        s = "CanopenBus: name=%s fd=%d" % (self.ifname,self.fd())
         return s
