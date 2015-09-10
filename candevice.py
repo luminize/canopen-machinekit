45c3812d73dad650f01d4789c19172b4b73b5bad
Michael Haberler <git@mah.priv.at>
2015-09-10 17:40:41 +0200

    CanDevice superclass
    subclasses by default inherit all methods and attributes
    or may choose to override them

---
+++ candevice.py
@@ -0,0 +1,27 @@
+''' can device class '''
+
+
+class CanDevice(object):
+
+    def __init__(self, node_id, parent_bus):
+        self.node_id = node_id
+        self.parent_bus = parent_bus
+        self.last_timestamp = None # last known timestamp
+        parent_bus.add_device(node_id, self)  # nb object reference, not name
+
+    def process(self, msg):
+        print "device %d: msg %s" % (self.node_id, msg)
+        self.last_timestamp = msg.timestamp
+        #print "device %d last known timestamp %f:" % (self.node_id, \
+        #                                               self.last_timestamp)
+
+    def timeout(self):
+        pass
+        #print "device %d: timeout" % (self.node_id)
+
+    # string representation of a CanDevice object:
+    def __str__(self):
+        s = "CanDevice "
+        s += "node_id=%d " % self.node_id
+        s += "parent=%s " % self.parent_bus.ifname
+        return s
